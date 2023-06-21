1bd222eed7ff83bdcdd931303d275e44ff076649
skajinet <skaji.net1@gmail.com>
2023-06-21 22:32:23 +0300

    Vladimir commit

---
+++ Assets/Scenes/Scripts/TestScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveTest();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveTest()
+    {
+        Debug.Log('faffaaf')
+    }
+}
