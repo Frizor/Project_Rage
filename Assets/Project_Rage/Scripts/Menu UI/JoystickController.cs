2aed2caf576ac37d2798d0922952a7fd9568cebb
Frizor <iv_93.93@mail.ru>
2023-07-05 04:00:12 +0300

    джойстик

---
+++ Assets/Project_Rage/Scripts/Menu UI/JoystickController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickController : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public RectTransform joystickBackground;
+    public RectTransform joystickHandle;
+
+    private Vector2 joystickPosition = Vector2.zero;
+    private bool isJoystickPressed = false;
+
+    private void Update()
+    {
+        if (isJoystickPressed)
+        {
+            // Обновление позиции игрового персонажа на основе данных сенсорного джойстика
+            // Можно вызвать методы движения или поворота персонажа здесь
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 position;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBackground, eventData.position, eventData.pressEventCamera, out position))
+        {
+            Vector2 clampedPosition = Vector2.ClampMagnitude(position, joystickBackground.rect.width * 0.5f);
+            joystickHandle.localPosition = clampedPosition;
+            joystickPosition = clampedPosition.normalized;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isJoystickPressed = true;
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isJoystickPressed = false;
+        joystickHandle.localPosition = Vector2.zero;
+        joystickPosition = Vector2.zero;
+    }
+
+    public Vector2 GetJoystickAxes()
+    {
+        return joystickPosition;
+    }
+}
+
+
+
+
+/*using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickController : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public RectTransform joystickBackground;
+    public RectTransform joystickHandle;
+
+    private Vector2 joystickPosition = Vector2.zero;
+    private bool isJoystickPressed = false;
+
+    private void Update()
+    {
+        if (isJoystickPressed)
+        {
+            // Обновление позиции игрового персонажа на основе данных сенсорного джойстика
+            // Можно вызвать методы движения или поворота персонажа здесь
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 position;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBackground, eventData.position, eventData.pressEventCamera, out position))
+        {
+            Vector2 clampedPosition = Vector2.ClampMagnitude(position, joystickBackground.rect.width * 0.5f);
+            joystickHandle.localPosition = clampedPosition;
+            joystickPosition = clampedPosition.normalized;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isJoystickPressed = true;
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isJoystickPressed = false;
+        joystickHandle.localPosition = Vector2.zero;
+        joystickPosition = Vector2.zero;
+    }
+}
+*/
--- Assets/Project_Rage/Scripts/Player/PlayerController.cs
+++ Assets/Project_Rage/Scripts/Player/PlayerController.cs
@@ -14,6 +14,167 @@ public class PlayerController : MonoBehaviour
     public float accelerationCooldown = 10f;
     public float rotationSpeed = 10f; // Speed of rotation on right-click
 
+    public bool useJoystickControl = true; // Enable or disable joystick control in the inspector
+
+    private NavMeshAgent _navMeshAgent;
+    private Camera _mainCamera;
+    private Vector3 _cameraOffset;
+    private bool _isAccelerating;
+    private float _originalSpeed;
+    private float _accelerationTimer;
+    private float _cooldownTimer;
+
+    private JoystickController _joystickController; // Reference to the JoystickController script
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _mainCamera = Camera.main;
+        _cameraOffset = _mainCamera.transform.position - transform.position;
+        _originalSpeed = _navMeshAgent.speed;
+        _accelerationTimer = 0f;
+        _cooldownTimer = 0f;
+
+        _joystickController = FindObjectOfType<JoystickController>();
+    }
+
+    private void LateUpdate()
+    {
+        if (useJoystickControl && _joystickController != null)
+        {
+            // Move and rotate the player with the joystick
+            MovePlayerWithJoystick();
+            RotatePlayerWithJoystick();
+        }
+        else
+        {
+            // Move and rotate the player with the mouse (for PC)
+            MovePlayerWithMouse();
+            RotatePlayerWithMouse();
+        }
+
+        // Update camera position and field of view
+        Vector3 cameraPosition = transform.position + _cameraOffset;
+        _mainCamera.transform.position = cameraPosition;
+        _mainCamera.transform.LookAt(transform.position);
+
+        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        float fieldOfView = _mainCamera.fieldOfView;
+        fieldOfView -= scrollWheel * zoomSpeed;
+        fieldOfView = Mathf.Clamp(fieldOfView, minFieldOfView, maxFieldOfView);
+        _mainCamera.fieldOfView = fieldOfView;
+
+        // Handle acceleration and cooldown
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!_isAccelerating && _cooldownTimer <= 0f)
+            {
+                StartAcceleration();
+            }
+        }
+
+        if (_isAccelerating)
+        {
+            _accelerationTimer -= Time.deltaTime;
+            if (_accelerationTimer <= 0f)
+            {
+                StopAcceleration();
+            }
+        }
+        else if (_cooldownTimer > 0f)
+        {
+            _cooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    private void MovePlayerWithMouse()
+    {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // Set the destination for NavMeshAgent
+                _navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    private void MovePlayerWithJoystick()
+    {
+        Vector2 joystickAxes = _joystickController.GetJoystickAxes();
+        if (joystickAxes.magnitude > 0.01f)
+        {
+            Vector3 movement = new Vector3(joystickAxes.x, 0f, joystickAxes.y);
+            movement.Normalize();
+
+            // Set the destination for NavMeshAgent
+            _navMeshAgent.SetDestination(transform.position + movement);
+        }
+    }
+
+    private void RotatePlayerWithMouse()
+    {
+        if (Input.GetMouseButton(1) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Vector3 targetDirection = hit.point - transform.position;
+                targetDirection.y = 0f;
+                if (targetDirection.magnitude > 0.01f)
+                {
+                    Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    private void RotatePlayerWithJoystick()
+    {
+        Vector2 joystickAxes = _joystickController.GetJoystickAxes();
+        if (joystickAxes.magnitude > 0.01f)
+        {
+            float angle = Mathf.Atan2(joystickAxes.x, joystickAxes.y) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, angle, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void StartAcceleration()
+    {
+        _isAccelerating = true;
+        _navMeshAgent.speed = accelerationSpeed;
+        _accelerationTimer = accelerationDuration;
+    }
+
+    private void StopAcceleration()
+    {
+        _isAccelerating = false;
+        _navMeshAgent.speed = _originalSpeed;
+        _cooldownTimer = accelerationCooldown;
+    }
+}
+
+
+
+/*using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject NavigationSurface;
+    public float cameraHeight = 5f;
+    public float minFieldOfView = 30f;
+    public float maxFieldOfView = 90f;
+    public float zoomSpeed = 5f;
+    public float accelerationSpeed = 1000f;
+    public float accelerationDuration = 0.5f;
+    public float accelerationCooldown = 10f;
+    public float rotationSpeed = 10f; // Speed of rotation on right-click
+
     private NavMeshAgent _navMeshAgent;
     private Camera _mainCamera;
     private Vector3 _cameraOffset;
@@ -120,3 +281,4 @@ public class PlayerController : MonoBehaviour
         _cooldownTimer = accelerationCooldown;
     }
 }
+*/
