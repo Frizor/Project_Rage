773452b5b3c624c31628f91478294870ac212e9c
Frizor <iv_93.93@mail.ru>
2023-07-06 22:32:38 +0300

    инвентарь

--- Assets/Project_Rage/Scripts/Menu UI/Pickup.cs
+++ Assets/Project_Rage/Scripts/Menu UI/Pickup.cs
@@ -9,6 +9,42 @@ public class Pickup : MonoBehaviour
 
     private void Start()
     {
+        Debug.Log("init");
+        inventoryManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryManager>();
+        //inventoryManager = player.GetComponent<InventoryManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventoryManager.slots.Length; i++)
+            {
+                if (inventoryManager.ifFull[i] == false)
+                {
+                    inventoryManager.ifFull[i] = true;
+                    Instantiate(slotButton, inventoryManager.slots[i].transform);
+                    Destroy(gameObject);
+                    Debug.Log("Item picked up and added to the inventory."); // Добавьте эту строку для отладки
+                    break;
+                }
+            }
+        }
+    }
+}
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private InventoryManager inventoryManager;
+    public GameObject slotButton;
+
+    private void Start()
+    {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         inventoryManager = player.GetComponent<InventoryManager>();
     }
@@ -30,4 +66,4 @@ public class Pickup : MonoBehaviour
             }
         }
     }
-}
+}*/
