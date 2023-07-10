dc0f56dd4e0a376d352efc83ec50aab88645a44c
Frizor <iv_93.93@mail.ru>
2023-07-10 07:57:30 +0300

    Объединение HP
    Объединение HP значений хп с Character в PlayerLifeManager

--- Assets/Project_Rage/Scripts/Player/PlayerLifeManager.cs
+++ Assets/Project_Rage/Scripts/Player/PlayerLifeManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class PlayerLifeManager : MonoBehaviour
@@ -75,8 +76,95 @@ public class PlayerLifeManager : MonoBehaviour
             playerHealthBarUI.SetHealth(currentHealth);
         }
     }
+
+    public void SetMaxHealth(float health)
+    {
+        maxHealth = health;
+        if (playerHealthBarUI != null)
+        {
+            playerHealthBarUI.SetMaxHealth(maxHealth);
+        }
+    }
 }
 
+/*using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLifeManager : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    private PlayerHealthBarUI playerHealthBarUI;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        playerHealthBarUI = FindObjectOfType<PlayerHealthBarUI>();
+
+        if (playerHealthBarUI != null)
+        {
+            playerHealthBarUI.SetMaxHealth(maxHealth);
+            playerHealthBarUI.SetHealth(currentHealth);
+        }
+    }
+
+    public float CurrentHealth
+    {
+        get { return currentHealth; }
+        set { currentHealth = value; }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0f)
+        {
+            currentHealth = 0f;
+            // Выполните действия, связанные с проигрышем или смертью игрока
+        }
+
+        if (playerHealthBarUI != null)
+        {
+            playerHealthBarUI.SetHealth(currentHealth);
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if (playerHealthBarUI != null)
+        {
+            playerHealthBarUI.SetHealth(currentHealth);
+        }
+    }
+
+    public void SetHealth(float health)
+    {
+        currentHealth = health;
+
+        if (currentHealth < 0f)
+        {
+            currentHealth = 0f;
+        }
+        else if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if (playerHealthBarUI != null)
+        {
+            playerHealthBarUI.SetHealth(currentHealth);
+        }
+    }
+}*/
 
 /*using UnityEngine;
 
